39322ca21dafcf64c06b318eab3a60ad9b519ebe
Axel Ancona Esselmann <axel@anconaesselmann.com>
2018-01-07 18:53:20 -0800

    Creates files for VC, VM and View

---
+++ xcc.data/View.swift
@@ -0,0 +1,24 @@
+//  Created by {author_name} on {date}.
+//  Copyright © {copyright_date} {copyright_name}. All rights reserved.
+//
+
+import UIKit
+
+class InitialView: UIView {
+    
+    init() {
+        super.init(frame: CGRect.zero)
+        backgroundColor = .white
+        
+        createLayoutConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func createLayoutConstraints() {
+
+    }
+
+}
---
+++ xcc.data/ViewModel.swift
@@ -0,0 +1,9 @@
+//  Created by {author_name} on {date}.
+//  Copyright © {copyright_date} {copyright_name}. All rights reserved.
+//
+
+import UIKit
+
+class {vm_name} {
+
+}
